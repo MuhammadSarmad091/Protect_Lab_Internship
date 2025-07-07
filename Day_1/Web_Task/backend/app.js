897754a7e0bd444efaf4b372826adc5f58a21652
Muhammad Sarmad <i220941@nu.edu.pk>
2025-07-07 15:02:55 +0500

    Build Express Backend Day_1

---
+++ Day_1/Web_Task/backend/app.js
@@ -0,0 +1,47 @@
+var express = require('express')
+var app = express()
+
+app.use(express.json())
+
+const roomRecords = [
+  // Bedroom records
+  [
+    { Name: "Bedroom", Temperature: 35.7, Humidity: 40, Lights: "On",  Fan: "Off", AC: "Off", Blinds: "On"  },
+    { Name: "Bedroom", Temperature: 30.3, Humidity: 45, Lights: "Off", Fan: "On",  AC: "Off", Blinds: "Off" },
+    { Name: "Bedroom", Temperature: 19.8, Humidity: 50, Lights: "On",  Fan: "Off", AC: "On",  Blinds: "On"  },
+    { Name: "Bedroom", Temperature: 21.0, Humidity: 42, Lights: "Off", Fan: "Off", AC: "Off", Blinds: "Off" },
+    { Name: "Bedroom", Temperature: 23.5, Humidity: 48, Lights: "On",  Fan: "On",  AC: "Off", Blinds: "On"  }
+  ],
+
+  // LivingRoom records
+  [
+    { Name: "LivingRoom", Temperature: 24.2, Humidity: 55, Lights: "On",  Fan: "Off", AC: "On",  Blinds: "Off" },
+    { Name: "LivingRoom", Temperature: 25.0, Humidity: 60, Lights: "Off", Fan: "Off", AC: "On",  Blinds: "Off" },
+    { Name: "LivingRoom", Temperature: 32.1, Humidity: 58, Lights: "On",  Fan: "On",  AC: "Off", Blinds: "On"  },
+    { Name: "LivingRoom", Temperature: 35.7, Humidity: 52, Lights: "Off", Fan: "Off", AC: "Off", Blinds: "Off" },
+    { Name: "LivingRoom", Temperature: 24.9, Humidity: 57, Lights: "On",  Fan: "Off", AC: "On",  Blinds: "On"  }
+  ],
+
+  // Kitchen records
+  [
+    { Name: "Kitchen", Temperature: 32.1, Humidity: 65, Lights: "On",  Fan: "On",  AC: "Off", Blinds: "Off" },
+    { Name: "Kitchen", Temperature: 27.3, Humidity: 70, Lights: "Off", Fan: "On",  AC: "On",  Blinds: "Off" },
+    { Name: "Kitchen", Temperature: 36.7, Humidity: 68, Lights: "On",  Fan: "Off", AC: "Off", Blinds: "On"  },
+    { Name: "Kitchen", Temperature: 39.7, Humidity: 66, Lights: "Off", Fan: "On",  AC: "Off", Blinds: "Off" },
+    { Name: "Kitchen", Temperature: 28.0, Humidity: 72, Lights: "On",  Fan: "On",  AC: "On",  Blinds: "On"  }
+  ]
+];
+
+const numRooms = 3;
+const numRecordsPerRoom = 5;
+
+app.get('/',(req,res)=>{
+  data = new Array(3);
+  for(let i = 0; i<numRooms; i++){
+    rand = Math.random() % numRecordsPerRoom;
+    data[i] = roomRecords[i][rand];
+  }
+  res.send(data);
+});
+
+module.exports = app;
---
+++ Day_1/Web_Task/frontend/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+/*
+{
+  Name : Bedroom
+  Temperature : 
+  Humidity : 
+  Lights :
+  Fan : 
+  AC :
+}
+
+*/
+
+function App() {
+  const [roomsData,setRoomsData] = useState([])
+  return (
+    <></>
+  );
+}
+
+export default App;
